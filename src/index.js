6c78b80059e5fbe1bc64daf3148cb2c514d60b0e
Atsighi Bright <brytebee@gmail.com>
2021-08-24 08:55:08 +0100

    Dynamically add values to page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 import './styles.css';
+
+let listItem = [
+    {
+        completed: false,
+        description: 'Learn how to use proper ES6 syntax',
+        index: 8
+    },
+    {
+        completed: false,
+        description: 'Use ES6 modules to write modular JavaScript',
+        index: 12
+    },
+    {
+        completed: false,
+        description: 'Use webpack to bundle JavaScript',
+        index: 6
+    }
+];
+
+listItem = listItem.sort((a, b) => {
+    return a.index - b.index;
+});
+  
+const populate = () => {
+    const page = document.getElementById('list');
+    listItem.forEach(element => {
+        const div = document.createElement('div');
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox', 'defaultChecked');
+        div.append(checkbox);
+        div.append(element.description);
+        div.classList.add('list-group-item');
+        page.append(div);
+    });
+};
+
+populate();

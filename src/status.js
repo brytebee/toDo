751eca55127b014df6fec377e9f6632d25616bdc
Atsighi Bright <brytebee@gmail.com>
2021-08-25 11:59:49 +0100

    Fix Linters

--- src/status.js
+++ src/status.js
@@ -1,16 +1,16 @@
-export const status = () => {
+export default function status() {
   const list = JSON.parse(localStorage.getItem('list'));
   const checkbox = Object.values(document.getElementsByName('checkbox'));
   checkbox.forEach((e, i) => {
     e.addEventListener('click', () => {
-     if (e.checked) {
-       list[i].completed = true;
-       e.parentElement.style.textDecoration = 'line-through underline';
-     } else {
-      list[i].completed = false;
-      e.parentElement.style.textDecoration = 'none';
-     }
-     localStorage.setItem('list', JSON.stringify(list))
-    })
-  })
+      if (e.checked) {
+        list[i].completed = true;
+        e.parentElement.style.textDecoration = 'line-through underline';
+      } else {
+        list[i].completed = false;
+        e.parentElement.style.textDecoration = 'none';
+      }
+      localStorage.setItem('list', JSON.stringify(list));
+    });
+  });
 };

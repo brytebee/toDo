ec762ce23e4dcc5d2f09ad0472319ea9fe6df4ac
Atsighi Bright <brytebee@gmail.com>
2021-08-26 11:54:56 +0100

    Display content

--- src/index.js
+++ src/index.js
@@ -1,53 +1,84 @@
 import './styles.css';
 import status from './status.js';
+import './crud.js';
 
-let listItem = [
-  {
-    completed: false,
-    description: 'Learn how to use proper ES6 syntax',
-    index: 8,
-  },
-  {
-    completed: false,
-    description: 'Use ES6 modules to write modular JavaScript',
-    index: 12,
-  },
-  {
-    completed: false,
-    description: 'Use webpack to bundle JavaScript',
-    index: 6,
-  },
-];
+let listItem = [];
 
-listItem = listItem.sort((a, b) => a.index - b.index);
-
-if (!localStorage.getItem('list')) {
-  localStorage.setItem('list', JSON.stringify(listItem));
-}
-const populate = () => {
-  let storeList;
-  if (localStorage.getItem('list')) {
-    storeList = JSON.parse(localStorage.getItem('list'));
+class List {
+  constructor(description, index) {
+      this.completed = false;
+      this.description = description;
+      this.index = listItem.length +1;
+      this.id = `id${this.index}`
   }
-  const page = document.getElementById('list');
-  storeList.forEach((element) => {
+}
+
+const add = () => {
+  document.getElementById('addBtn').addEventListener('click', () => {
+    const newItem = new List(`${document.getElementById('input').value}`, listItem.length);
+    listItem.push(newItem);
+    localStorage.setItem('list', JSON.stringify(listItem));
+});
+}
+
+add();
+
+const display = () => {
+  const page = document.getElementById('list')
+  const list = JSON.parse(localStorage.getItem('list'));
+  list.forEach((e) => {
     const div = document.createElement('div');
     const checkbox = document.createElement('input');
-    if (element.completed) {
+    const removeBtn = document.createElement('a');
+    removeBtn.innerHTML = '<i class="fas fa-trash"></i>';
+    removeBtn.id = e.id;
+    if (e.completed) {
       (div.style.textDecoration = 'underline line-through');
     } else {
       (div.style.textDecoration = 'none');
     }
     checkbox.setAttribute('type', 'checkbox');
     checkbox.name = 'checkbox';
-    checkbox.checked = element.completed;
+    checkbox.checked = e.completed;
     div.append(checkbox);
-    div.append(element.description);
+    div.append(e.description);
+    div.append(removeBtn);
     div.classList.add('list-group-item');
     page.append(div);
   });
-};
+}
 
-populate();
+display();
 
-status();
+// listItem = listItem.sort((a, b) => a.index - b.index);
+
+// if (!localStorage.getItem('list')) {
+//   localStorage.setItem('list', JSON.stringify(listItem));
+// }
+// const populate = () => {
+//   let storeList;
+//   if (localStorage.getItem('list')) {
+//     storeList = JSON.parse(localStorage.getItem('list'));
+//   }
+//   const page = document.getElementById('list');
+//   storeList.forEach((element) => {
+//     const div = document.createElement('div');
+//     const checkbox = document.createElement('input');
+//     if (element.completed) {
+//       (div.style.textDecoration = 'underline line-through');
+//     } else {
+//       (div.style.textDecoration = 'none');
+//     }
+//     checkbox.setAttribute('type', 'checkbox');
+//     checkbox.name = 'checkbox';
+//     checkbox.checked = element.completed;
+//     div.append(checkbox);
+//     div.append(element.description);
+//     div.classList.add('list-group-item');
+//     page.append(div);
+//   });
+// };
+
+// populate();
+
+// status();
